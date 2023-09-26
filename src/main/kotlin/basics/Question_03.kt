76098bef0e08db6c1e21469f79bb6cd773275062
asharful07 <asharfulkhan202@gmail.com>
2023-09-26 22:39:53 +0530

    Question_03

---
+++ src/main/kotlin/basics/Question_03.kt
@@ -0,0 +1,10 @@
+
+
+// Calculate the Volume of Sphere
+
+package basics
+fun main() {
+    val radius = 5.5F
+    val volume = ((4 / 3) * 3.14F * (radius * radius * radius))
+    println("The volume of Sphere is : $volume")
+}
