5fef574b59b3dac73e60656d9bf364d6fe574668
asharful07 <asharfulkhan202@gmail.com>
2023-09-26 22:47:54 +0530

    Question_04

---
+++ src/main/kotlin/basics/Question_04.kt
@@ -0,0 +1,10 @@
+
+// convert the given string into uppercase and reverse order
+
+package basics
+fun main(){
+    val string = "Asharful"
+    println("Given string: $string")
+    println("Reverse string: ${string.toUpperCase().reversed()}")
+
+}
