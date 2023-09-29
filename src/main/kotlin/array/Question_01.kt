f2234f181c82dd33f5136caca8471ebbaa5db00a
asharful07 <asharfulkhan202@gmail.com>
2023-09-29 11:36:48 +0530

    Question_01

---
+++ src/main/kotlin/array/Question_01.kt
@@ -0,0 +1,12 @@
+
+// Print the Given Array
+
+package array
+fun main(){
+    val arr = arrayOf("Hi","Hello","Hey")
+    var i=0
+    while (i<arr.size){
+        println(arr[i])
+        i++
+    }
+}
