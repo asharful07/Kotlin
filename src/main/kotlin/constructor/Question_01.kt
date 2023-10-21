0904d4089fb3c71a84ff3cc02343b76e703f7c0e
asharful07 <asharfulkhan202@gmail.com>
2023-10-21 20:22:14 +0530

    Question_01

---
+++ src/main/kotlin/constructor/Question_01.kt
@@ -0,0 +1,20 @@
+package constructor
+class Calculator(a: Int, b: Int) {
+    var num1 = a
+    var num2 = b
+
+    fun add():Int{
+        return num1+num2
+    }
+
+    fun multiply():Int{
+        return num1*num2
+    }
+}
+fun main() {
+    val obj = Calculator(5,10)
+    println(obj.num1)
+    println(obj.num2)
+    println(obj.add())
+    println(obj.multiply())
+}
