94672864918ad77c045704a333eebd699359c64f
asharful07 <asharfulkhan202@gmail.com>
2023-12-08 22:58:33 +0530

    Question_08

---
+++ src/main/kotlin/Function/Question_08.kt
@@ -0,0 +1,13 @@
+
+//Print the details of student by function
+
+package Function
+fun student( name: String , grade: Char , roll_no: Int) {
+    println("Name of the student is : $name")
+    println("Grade of the student is: $grade")
+    println("Roll no of the student is: $roll_no")
+
+}
+fun main(args: Array<String>) {
+    student("Aaman",'A',10)
+}
