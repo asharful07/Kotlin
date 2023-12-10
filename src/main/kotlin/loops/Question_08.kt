019eec840f8115869b7ff528e108bd4794da5c3e
asharful07 <asharfulkhan202@gmail.com>
2023-12-10 18:53:55 +0530

    Question_08

---
+++ src/main/kotlin/loops/Question_08.kt
@@ -0,0 +1,10 @@
+
+//Iterate through Range from top to down with using downTo and step 3
+
+package loops
+fun main(args: Array<String>)
+{
+    for (i in 10 downTo 1 step 3) {
+        print("$i ")
+    }
+}
