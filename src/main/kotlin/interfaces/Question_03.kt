81f337771d52945a5d4af14e4feee92e2562012b
asharful07 <asharfulkhan202@gmail.com>
2023-12-11 15:44:29 +0530

    Question_03

---
+++ src/main/kotlin/interfaces/Question_03.kt
@@ -0,0 +1,19 @@
+package interfaces
+
+interface InterfaceProperties {
+    val a : Int
+    val b : String
+        get() = "Hello"
+}
+
+class PropertiesDemo : InterfaceProperties {
+    override val a : Int = 5000
+    override val b : String = "Property Overridden"
+}
+
+fun main()
+{
+    val x = PropertiesDemo()
+    println(x.a)
+    println(x.b)
+}
