c683b28be5f3f28b8328676261ee591e74fc3e12
asharful07 <asharfulkhan202@gmail.com>
2023-12-12 19:40:17 +0530

    Question_04

---
+++ src/main/kotlin/interfaces/Question_04.kt
@@ -0,0 +1,37 @@
+
+
+//Calculate the Area and Perimeter of Rectangle By interface
+
+package interfaces
+
+interface Dimensions {
+    val length : Double
+    val breadth : Double
+}
+interface CalculateParameters : Dimensions {
+    fun area()
+    fun perimeter()
+}
+class XYZ : CalculateParameters {
+    override val length : Double
+        get() = 10.0
+    override val breadth : Double
+        get()= 15.0
+
+    override fun area()
+    {
+        println("Area is ${length * breadth}")
+    }
+
+    override fun perimeter()
+    {
+        println("Perimeter is ${2*(length+breadth)}")
+    }
+}
+
+fun main()
+{
+    val obj = XYZ()
+    obj.area()
+    obj.perimeter()
+}
