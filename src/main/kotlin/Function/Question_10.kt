2f2440b3090bfc8b4b38cf4337db8fc0bbdd92a3
asharful07 <asharfulkhan202@gmail.com>
2023-12-16 11:04:38 +0530

    Question_10

---
+++ src/main/kotlin/Function/Question_10.kt
@@ -0,0 +1,13 @@
+
+
+//Multiplication of Two number by function
+
+package Function
+fun mul(a: Int, b: Int): Int {
+    val number = a.times(b)
+    return number
+}
+fun main() {
+    val result = mul(5,5)
+    println("The multiplication of two numbers is: $result")
+}
