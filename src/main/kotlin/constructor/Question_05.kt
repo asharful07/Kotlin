bd0b8c8abb186a02940119455ff9736b4e6273a8
asharful07 <asharfulkhan202@gmail.com>
2023-12-17 10:40:36 +0530

    Question_05

---
+++ src/main/kotlin/constructor/Question_05.kt
@@ -0,0 +1,24 @@
+package constructor
+class employee(emp_id : Int = 100 , emp_name: String = "abc") {
+    val id: Int
+    var name: String
+
+    // initializer block
+    init {
+        id = emp_id
+        name = emp_name
+
+        println("Employee id is: $id, ")
+        println("Employee name: $name")
+        println()
+    }
+}
+
+fun main() {
+    val emp = employee(18018, "Sagnik")
+
+    val emp2 = employee(11011)
+
+    val emp3 = employee()
+}
+
